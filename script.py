a262a7939bb3432df7ba87481b9541ad96d342f9
Topher Brennan <challquist@gmail.com>
2022-02-01 19:30:57 -0800

    Unsuccessful attempt to incorporate DFRPG.

--- script.py
+++ script.py
@@ -4,7 +4,8 @@ MAGIC_FILENAME = "GURPS Magic 4e.gdf"
 WIZARDRY_REFINED_FILENAME = "GURPS Dungeon Fantasy Pyramid 3-60 Wizardry Redefined.gdf"
 ADVENTURERS_FILENAME = "GURPS Dungeon Fantasy  1 - Adventurers.GDF"
 NEXT_LEVEL_FILENAME = "GURPS Dungeon Fantasy  3 - Next Level.gdf"
-CLERICS_FILENAME = 'GURPS Dungeon Fantasy  7 - Clerics.gdf'
+CLERICS_FILENAME = "GURPS Dungeon Fantasy  7 - Clerics.gdf"
+DFRPG_FILENAME = "Dungeon Fantasy RPG.gdf"
 
 SPELL_PATTERN = r'(?<=SP:)[\w \(\)\[\]]+(?=\n)'
 DELETE_PATTERN = r'(?<=#Delete "SP:)[\w \(\)\[\]]+(?=")'
@@ -13,6 +14,7 @@ DRUIDIC_PATTERN = r'(?<=SP:)[\w \(\)\[\]]+Druidic\)'
 UNHOLY_PATTERN = r'(?<=SP:)[\w \(\)\[\]]+Unholy\)'
 CLONE_PATTERN = r'(?<=#Clone "SP:)[\w \(\)\[\]]+(?=")'
 SPELL_BLOCK_PATTERN = r'.+, type.+\n.+\n.+\n?.*Prereq Count: \d.+\)'
+DFRPG_SPELL_BLOCK_PATTERN = r'.+,\n\tcat.+(?:\n.+)+Prerequisites: .+'
 PREREQ_COUNT_PATTERN = r'(?<=Prereq Count: )\d+'
 PREREQ_PATTERN = r'(?<=Prerequisites: ).+(?=\))'
 
@@ -26,6 +28,22 @@ def from_different_system(spell_name):
 
 def print_set(s):
     print(f'{sorted(list(s))} ({len(s)} items)')
+
+def get_prereq_map(file_contents, pattern=SPELL_BLOCK_PATTERN):
+    spell_blocks = re.findall(pattern, file_contents)
+    prereq_map = {}
+    for spell_block in spell_blocks:
+        spell_name = spell_block.split(',')[0]
+        # TODO: improve this to handle "or"s.
+        prereq_count = re.search(PREREQ_COUNT_PATTERN, spell_block).group()
+        prereqs_match = re.search(PREREQ_PATTERN, spell_block)
+        if prereqs_match:
+            prereqs = set(prereqs_match.group().split(', '))
+        else:
+            prereqs = set()
+        prereq_map[spell_name] = [prereqs, prereq_count]
+    return prereq_map
+
 
 magic = open(MAGIC_FILENAME).read()
 all_spells = set([spell_name for spell_name in re.findall(SPELL_PATTERN, magic) if not from_different_system(spell_name)])
@@ -43,21 +61,11 @@ evil_cleric_spells = set([remove_word(spell_name, 'Unholy') for spell_name in re
 clerics = open(CLERICS_FILENAME).read()
 specialist_cleric_spells = set(re.findall(CLONE_PATTERN, clerics))
 
-spell_blocks = re.findall(SPELL_BLOCK_PATTERN, magic)
-prereq_map = {}
-for spell_block in spell_blocks:
-    spell_name = spell_block.split(',')[0]
-    # TODO: improve this to handle "or"s.
-    prereq_count = re.search(PREREQ_COUNT_PATTERN, spell_block).group()
-    prereqs_match = re.search(PREREQ_PATTERN, spell_block)
-    if prereqs_match:
-        prereqs = set(prereqs_match.group().split(', '))
-    else:
-        prereqs = set()
-    prereq_map[spell_name] = [prereqs, prereq_count]
+prereq_map = get_prereq_map(magic)
 
 all_cleric_spells = standard_cleric_spells.union(evil_cleric_spells).union(specialist_cleric_spells)
 druid_exclusives = druid_spells - all_cleric_spells
+standard_cleric_exclusives = standard_cleric_spells - evil_cleric_spells - specialist_cleric_spells
 evil_cleric_exclusives = evil_cleric_spells - standard_cleric_spells - specialist_cleric_spells
 wizard_exclusives = all_spells - wizardry_refined_forbidden - all_cleric_spells - druid_spells
 
@@ -67,13 +75,28 @@ for spell_name in wizard_exclusives:
             (prereq_map[spell_name][0].intersection(all_cleric_spells) or \
             prereq_map[spell_name][1] == '0'):
         style_spell_candidates.add(spell_name)
-# print_set(style_spell_candidates)
+"""
+print_set(style_spell_candidates)
 
 for candidate in sorted(list(style_spell_candidates)):
     if 'Divination' not in candidate and 'Command Spirit' not in candidate:
         print(f'{candidate}: {prereq_map[candidate][1]}')
+"""
 
 # print_set(evil_cleric_exclusives)
+# print_set(standard_cleric_exclusives)
 # print_set(wizard_exclusives)
 # print_set(all_cleric_spells)
 # print_set(druid_exclusives)
+
+dfrpg = open(DFRPG_FILENAME).read()
+dfrpg_prereq_map = get_prereq_map(dfrpg, DFRPG_SPELL_BLOCK_PATTERN)
+# print(dfrpg_prereq_map)
+
+for spell, prereqs in dfrpg_prereq_map.items():
+    standard_prereqs = prereq_map[spell]
+    if True: 
+    # if prereqs[0] != standard_prereqs[0]:
+        print(spell)
+        print(standard_prereqs)
+        print(prereqs)
