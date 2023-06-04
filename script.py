2d4376b4849b65e8f17ea6f6814683b092b3a88d
Topher Brennan <topherbrennan@Tophers-Air.attlocal.net>
2023-06-04 13:22:18 -0700

    Got graphviz working sort-of (TODO: make charts for single colleges).

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 MAGIC_FILENAME = "GURPS Magic 4e.gdf"
 WIZARDRY_REFINED_FILENAME = "GURPS Dungeon Fantasy Pyramid 3-60 Wizardry Redefined.gdf"
@@ -69,6 +70,7 @@ standard_cleric_exclusives = standard_cleric_spells - evil_cleric_spells - speci
 evil_cleric_exclusives = evil_cleric_spells - standard_cleric_spells - specialist_cleric_spells
 wizard_exclusives = all_spells - wizardry_refined_forbidden - all_cleric_spells - druid_spells
 
+# I don't remembrer what this was supposed to accomplish.
 style_spell_candidates = set()
 for spell_name in wizard_exclusives:
     if prereq_map.get(spell_name) and \
@@ -92,7 +94,9 @@ for candidate in sorted(list(style_spell_candidates)):
 dfrpg = open(DFRPG_FILENAME).read()
 dfrpg_prereq_map = get_prereq_map(dfrpg, DFRPG_SPELL_BLOCK_PATTERN)
 # print(dfrpg_prereq_map)
+# print(prereq_map)
 
+""" 
 for spell, prereqs in dfrpg_prereq_map.items():
     standard_prereqs = prereq_map[spell]
     if True: 
@@ -100,3 +104,27 @@ for spell, prereqs in dfrpg_prereq_map.items():
         print(spell)
         print(standard_prereqs)
         print(prereqs)
+"""
+
+def to_graphviz(prereq_map):
+    reversed_prereq_map = defaultdict(list)
+    for spell, info in prereq_map.items():
+        for prereq in info[0]:
+            if 'Magery' not in prereq:
+                reversed_prereq_map[prereq].append(spell)
+            else:
+                pass # TODO: Indicate Magery prerequisites in some other way.
+
+    result = 'digraph {\n'
+    for prereq, prereq_for in reversed_prereq_map.items():
+        safe_prereq = prereq.replace('"', "'")
+        result += f'    "{safe_prereq}" -> '
+        result += '{ '
+        result += ', '.join([f'"{spell}"' for spell in prereq_for])
+        result += ' }\n'
+    result += '}'
+
+    return result
+
+print(to_graphviz(prereq_map))
+
